a71835e29d2db54bd50c42d2c8f2cf3a97f48e89
임수연 <81746616+dlatndus@users.noreply.github.com>
2023-05-26 10:20:17 +0900

    파일 업로드와 로그인 연결

--- webServer/app15-1.js
+++ webServer/app15-1.js
@@ -1,9 +1,3 @@
-/**
- * session 사용하기
- * 
- * 웹브라우저에서 아래 주소의 페이지를 열고 웹페이지에서 요청
- *    http://localhost:3000/login2.html
- */
 
 // Express 기본 모듈 불러오기
 var express = require('express')
@@ -16,26 +10,28 @@ var bodyParser = require('body-parser')
   , static = require('serve-static')
   , errorHandler = require('errorhandler');
 
+var multer = require('multer');
+var fs = require('fs');
+var cors = require('cors');
+
 // 에러 핸들러 모듈 사용
 var expressErrorHandler = require('express-error-handler');
-
 // Session 미들웨어 불러오기
 var expressSession = require('express-session');
-
-
-// 익스프레스 객체 생성
 var app = express();
 
 // 기본 속성 설정
 app.set('port', process.env.PORT || 3000);
+
+app.use(static(path.join(__dirname, 'public')));
+app.use('/uploads', static(path.join(__dirname, 'uploads')));
+app.use(cors());
 
 // body-parser를 이용해 application/x-www-form-urlencoded 파싱
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // body-parser를 이용해 application/json 파싱
 app.use(bodyParser.json())
-
-app.use(static(path.join(__dirname, 'public')));
 
 // cookie-parser 설정
 app.use(cookieParser());
@@ -46,6 +42,25 @@ app.use(expressSession({
 	resave:true,
 	saveUninitialized:true // session정보를 저장할때 저장할 파일을
 }));
+
+var storage = multer.diskStorage({
+    destination:function(req, file, callback){
+        callback(null, 'uploads')
+    },
+    filename:function(req, file, callback){
+        var extension = path.extname(file.originalname);
+        var basename = path.basename(file.originalname);
+        callback(null, basename + Date.now() + extension);
+    }
+});
+
+var upload = multer({
+    storage:storage,
+    limits:{
+        files:12,
+        fileSize:1024*1024*1024
+    }
+});
 
 
 // 라우터 사용하여 라우팅 함수 등록
@@ -75,10 +90,53 @@ router.route('/process/login').post(function(req, res) {
 		res.write('<div><p>Param id : ' + paramId + '</p></div>');
 		res.write('<div><p>Param password : ' + paramPassword + '</p></div>');
 		res.write("<br><br><button><a href='/process/product'>상품 페이지로 이동하기</a></button>");  
+		res.write("<button><a href='/photomulti3109.html'>파일업로드로 이동하기</a></button>"); 
 		//'/process/product'>상품 => 108라인의  router.route('/process/product').get으로 연결됨
 		//res.write("<br><br><a href='/pbulic/product.html'>상품 페이지로 이동하기</a>"); 와 동일 
 		res.end();
 	}
+});
+
+
+router.route('/process/photo12').post(upload.array('photo12', 12), function(req, res) {
+    console.log('/process/photo12 호출함.');
+    res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+    try{
+        var files = req.files;
+
+        // 현재의 파일 정보를 저장할 변수 선언
+        var originalname = '',
+            filename = '',
+            mimetype = '',
+            size = 0;
+
+            if (Array.isArray(files)) { 
+                // 배열에 들어가 있는 경우 (설정에서 1개의 파일도 배열에 넣게 했음)
+                console.log("배열에 들어있는 파일 갯수 : %d", files.length);
+           			
+				for (var index = 0; index < files.length; index++) {
+					console.dir('#===== 업로드된 '+ (index+1) +' 번째 파일 정보 =====#')
+					originalname = files[index].originalname;
+					filename = files[index].filename;
+					mimetype = files[index].mimetype;
+					size = files[index].size;
+					console.log('현재 파일 정보 : ' + originalname + ', ' + filename + ', '
+					+ mimetype + ', ' + size);
+			
+					// 클라이언트에 응답 전송
+					res.write( '<h3> 임수연님 '+(index+1)+' 번째 파일 업로드 성공</h3>');
+					res.write('<hr/>');
+					res.write('<p>원본 파일명 : ' + originalname + '<br> -> 저장 파일명 : ' + filename + '</p>');
+					res.write('<p>MIME TYPE : ' + mimetype + '</p>');
+					res.write('<p>파일 크기 : ' + size + '</p>');
+					res.end();
+				}
+				res.write("<br><br><button><a href='/process/product'>상품 페이지로 이동하기</a></button>");
+            }
+    }
+    catch(err){
+        console.dir(err.stack);
+    }
 });
 
 // 로그아웃 라우팅 함수 - 로그아웃 후 세션 삭제함 => photo.html의 <button type=button><a href='/process/logout'>로그아웃하기</a></button>
@@ -113,6 +171,7 @@ router.route('/process/product').get(function(req, res) {
 		res.redirect('/login2.html');
 	}
 });
+
 
 
  app.get('/', function (req, res) {
